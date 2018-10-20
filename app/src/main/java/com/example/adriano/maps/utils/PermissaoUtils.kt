896257d5f1d5fcbc32cae5b3324181616d3ff13b
adriano <adrianofpinheiro@outlook.com>
2018-10-20 12:39:47 -0300

    Kotlin - Android Utilização de Maps

---
+++ app/src/main/java/com/example/adriano/maps/utils/PermissaoUtils.kt
@@ -0,0 +1,28 @@
+package com.example.adriano.maps.utils
+
+
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.os.Build
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+
+object PermissaoUtils {
+
+    fun validaPermissao(permissoes: Array<String>, activity: Activity, requestCode: Int): Boolean {
+        if(Build.VERSION.SDK_INT >= 23) {
+
+            val listaPermissao = ArrayList<String>()
+
+            for(permissao in permissoes) {
+                val naoTemPermissao = ContextCompat.checkSelfPermission(activity, permissao) == PackageManager.PERMISSION_DENIED
+                if(naoTemPermissao) listaPermissao.add(permissao)
+            }
+//GLOARIA A DEUS
+            if(listaPermissao.isEmpty()) return true
+            val novasPermissoes = listaPermissao.toTypedArray()
+            ActivityCompat.requestPermissions(activity, novasPermissoes, requestCode)
+        }
+        return true
+    }
+}
